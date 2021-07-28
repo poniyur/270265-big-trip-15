0cd4f3109e25171797c1029146b86fc0e33fa07c
poltorak.n <94poniyur@mail.ru>
2021-07-28 16:32:01 +1000

    Настройка сервера для разработки (конфигурация)
    1. Установил из npm пакет webpack-dev-server
    2. Описал настройки сервера в webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
   },
   devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  },
 };
